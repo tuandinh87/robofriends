aa1c0a665dc30ef8e9e65dbd9f7df530959d0400
tuandinh87 <letuandinh@gmail.com>
2020-07-28 13:42:18 +1200

    Second Commit

---
+++ src/Hello.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import 'tachyons';
+import './Hello.css';
+
+class Hello extends React.Component {
+	render() {
+		return (
+			<div className = "f1 tc bg-yellow">
+			<h1 className = "bg-green">Hello Everyone</h1>
+			<p>Welcome to React</p>
+			</div>
+			)
+
+	}
+
+}
+
+export default Hello;
